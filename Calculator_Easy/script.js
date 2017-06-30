0a94fd61cc5486f0934c5b9d01f1c1daa75596c4
Christian Galang <xtn126@gmail.com>
2017-06-30 04:07:02 +0000

    Draft of scientific calculator

--- Calculator_Easy/script.js
+++ Calculator_Easy/script.js
@@ -2,31 +2,31 @@ document.getElementById('add').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
     var number2 = parseFloat(document.getElementById('num2').value);
     var final = number1 + number2;
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('subtract').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
     var number2 = parseFloat(document.getElementById('num2').value);
     var final = number1 - number2;
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('multiply').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
     var number2 = parseFloat(document.getElementById('num2').value);
     var final = number1 * number2;
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('divide').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
     var number2 = parseFloat(document.getElementById('num2').value);
     var final = number1 / number2;
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('carrot').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
     var number2 = parseFloat(document.getElementById('num2').value);
     var final = Math.pow(number1, number2);
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('factorial').onclick = function() {
     var number1 = parseFloat(document.getElementById('num1').value);
@@ -39,10 +39,10 @@ document.getElementById('factorial').onclick = function() {
         }
     }
     var final = factorial(number1);
-    document.getElementById('output').innerHTML += final;
+    document.getElementById('output').innerHTML = final;
 }
 document.getElementById('clear').onclick = function() {
     document.getElementById('num1').value = "";
     document.getElementById('num2').value = "";
-    document.getElementById('output').innerHTML = "Output: "
+    document.getElementById('output').innerHTML = ""
 }
--- scientific_calculator_draft/script.js
+++ scientific_calculator_draft/script.js
@@ -1,6 +1,9 @@
+// Clear button
 document.getElementById('clear').onclick = function() {
     document.getElementById('input-box').value = "";
 }
+
+// Delete button
 document.getElementById('delete').onclick = function() {
     var equation = document.getElementById('input-box').value;
     var arrEquation = equation.split('');
@@ -8,6 +11,8 @@ document.getElementById('delete').onclick = function() {
     equation = arrEquation.join('');
     document.getElementById('input-box').value = equation;
 }
+
+// Number buttons
 document.getElementById('one').onclick = function() {
     document.getElementById('input-box').value += '1';
 }
@@ -38,6 +43,8 @@ document.getElementById('nine').onclick = function() {
 document.getElementById('zero').onclick = function() {
     document.getElementById('input-box').value += '0';
 }
+
+// Math operator buttons
 document.getElementById('carrot').onclick = function() {
     document.getElementById('input-box').value += '^';
 }
@@ -53,38 +60,83 @@ document.getElementById('subtract').onclick = function() {
 document.getElementById('add').onclick = function() {
     document.getElementById('input-box').value += '+';
 }
+
+// Equal button
 document.getElementById('equals').onclick = function() {
     var final = 0;
     var equation = document.getElementById('input-box').value;
     var arrEquation = equation.split('');
+    function mathOperator(type, place) {
+        this.type = type;
+        this.place = place;
+    }
+    var arrOperators = new Array();
+    var count = 0;
     for (var i = 0; i < arrEquation.length; i++) {
         switch (arrEquation[i]) {
             case '+':
-                equation = arrEquation.join('');
-                final = (parseFloat(equation.substring(0, i)) + (parseFloat(equation.substring(i+1, equation.length))));
-                i = arrEquation.length;
+                arrOperators[count] = new mathOperator("addition", i);
+                count += 1;
                 break;
             case '^':
-                equation = arrEquation.join('');
-                final = Math.pow((parseFloat(equation.substring(0, i))), (parseFloat(equation.substring(i+1, equation.length))));
-                i = arrEquation.length;
+                arrOperators[count] = new mathOperator("power", i);
+                count += 1;
                 break;
             case '/':
-                equation = arrEquation.join('');
-                final = (parseFloat(equation.substring(0, i)) / (parseFloat(equation.substring(i+1, equation.length))));
-                i = arrEquation.length;
+                arrOperators[count] = new mathOperator("division", i);
+                count += 1;
                 break;
             case '*':
-                equation = arrEquation.join('');
-                final = (parseFloat(equation.substring(0, i)) * (parseFloat(equation.substring(i+1, equation.length))));
-                i = arrEquation.length;
+                arrOperators[count] = new mathOperator("multiplication", i);
+                count += 1;
                 break;
             case '-':
-                equation = arrEquation.join('');
-                final = (parseFloat(equation.substring(0, i)) - (parseFloat(equation.substring(i+1, equation.length))));
-                i = arrEquation.length;
+                arrOperators[count] = new mathOperator("subtraction", i);
+                count += 1;
                 break;
         }
     }
-    document.getElementById('input-box').value = final;
+    var tempNum;
+    var tempStr;
+    var change;
+    var check;
+    count = 0;
+    equation = arrEquation.join('');
+    for (var i = 0; i < arrOperators.length; i++) {
+        switch (arrOperators[i].type) {
+            case "addition":
+                // Get product of first equation and store in variable "temp"
+                tempNum = (parseFloat(equation.substring(count, arrOperators[i].place)) + (parseFloat(equation.substring(arrOperators[i].place + 1, arrOperators[i + 1].place))));
+                tempStr = tempNum.toString();
+                alert(tempStr);
+                
+                // Convert string to char array and delete everything until the place of the next operator
+                arrEquation = equation.split('');
+                for (var j = 0; j < arrOperators[i + 1].place; j++) {
+                    arrEquation[j] = '';
+                }
+                
+                // Move remaining equation back to beginning of the array
+                equation = arrEquation.join('');
+                arrEquation = equation.split('');
+                
+                // Move remaining equation forward to the length of temp
+                for (var k = arrEquation.length - 1; k >= 0; k--) {
+                    change = arrEquation[k];
+                    arrEquation[k] = '';
+                    arrEquation[k + tempStr.length] = change;
+                }
+                
+                // Place temp at beginning of equation and convert back to string
+                for (var l = 0; l < tempStr.length; l++) {
+                    arrEquation[l] = tempStr.substring(l, l + 1);
+                }
+                equation = arrEquation.join('');
+                
+                // Change places of math operators and output
+                arrOperators[i + 1].place = tempStr.length;
+                break;
+        }
+    }
+    document.getElementById('input-box').value = equation;
 }
